e623386aa091848ac843820828a169789398a621
Roberto Jimenez <robertojj2006@gmail.com>
2025-03-28 08:46:26 +0000

    Archivo Punto

--- geometria1/punto_constructor.py
+++ geometria1/punto_constructor.py
@@ -5,3 +5,27 @@ class Punto:
     
     def __str__(self):
         return f"({self.x}, {self.y})"
+    
+    def cuadrante(self):
+        if self.x > 0 and self.y > 0:
+            return "Primer cuadrante"
+        elif self.x < 0 and self.y > 0:
+            return "Segundo cuadrante"
+        elif self.x < 0 and self.y < 0:
+            return "Tercer cuadrante"
+        elif self.x > 0 and self.y < 0:
+            return "Cuarto cuadrante"
+        elif self.x == 0 and self.y != 0:
+            return "Eje Y"
+        elif self.y == 0 and self.x != 0:
+            return "Eje X"
+        else:
+            return "Origen"
+    
+    def vector(self, otro_punto):
+        return (otro_punto.x - self.x, otro_punto.y - self.y)
+
+import math
+
+def calcular_distancia(punto1, punto2):
+    return math.sqrt((punto2.x - punto1.x)**2 + (punto2.y - punto1.y)**2)
--- geometria1/vector.py
+++
@@ -1,2 +0,0 @@
-def calcular_vector(punto1, punto2):
-    return (punto2.x - punto1.x, punto2.y - punto1.y)
